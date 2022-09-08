7e759089d447df5c75e1e94175791595308a4eb7
TheUnknown550 <mattcosh06@gmail.com>
2022-09-08 09:16:05 +0700

    Loudest freq

--- GraphsConvert/(Amp,Signal)/FR_V1.py
+++ GraphsConvert/(Amp,Signal)/FR_V1.py
@@ -2,10 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import wave, sys
-import librosa
-from scipy.io import wavfile
-from os import path
-from pydub import AudioSegment
+
 
 
 audio = 'C:/Users/mattc/OneDrive/Documents/Project/CSM/Testing/AITest/A22/Test7.wav'
@@ -13,7 +10,7 @@ audio = 'C:/Users/mattc/OneDrive/Documents/Project/CSM/Testing/AITest/A22/Test7.
 raw = wave.open(audio)
 signal = raw.readframes(-1)
 signal = np.frombuffer(signal, dtype ="int16")
-db = librosa.amplitude_to_db(abs(signal))
-
-plt.plot(abs(signal))
+spectrum = np.fft.fft(signal)
+frequencies = np.fft.fftfreq(len(spectrum))
+plt.plot(frequencies,spectrum)
 plt.show()
